f90c46e801d4703fd5db87045c9f83c1e1d9cb8a
YudanMaulana <yudanmaulana21@gmail.com>
2024-06-28 23:18:02 +0700

    first commit right

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+
+
+  return (
+    <>
+      <div className='bg-blue-900 p-10 flex justify-center'>
+        <p className='text-slate-100 leading-relaxed'>This is First day coding with react typescript right</p>
+      </div>
+    </>
+  )
+}
+
+export default App
